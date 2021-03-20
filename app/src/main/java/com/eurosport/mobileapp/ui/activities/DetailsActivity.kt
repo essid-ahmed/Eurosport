16fe8b59e626ff93c6a1222e888141287cb89992
aessid <ahmed.essid@saur.com>
2021-03-20 14:01:47 +0100

    fix UI

--- app/src/main/java/com/eurosport/mobileapp/ui/activities/DetailsActivity.kt
+++ app/src/main/java/com/eurosport/mobileapp/ui/activities/DetailsActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import com.eurosport.domain.models.Story
 import com.eurosport.mobileapp.R
 import com.eurosport.mobileapp.utils.Constants
+import com.joooonho.SelectableRoundedImageView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.activity_details.view.*
@@ -19,10 +20,13 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     private fun setUpUi(story: Story) {
-        Picasso.get().load(story.image).into(item_details.item_picture)
+        var imageView  = item_details.item_picture
+        imageView.setCornerRadiiDP(0F,0F,0F,0F)
+        Picasso.get().load(story.image).into(imageView)
         item_details.title.text = story.title
         item_details.sport.text=story.sport.name
         item_details.author_view.text= getString(R.string.author, story.author, "0 minutes")
         description.text=story.teaser
+        back.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/com/eurosport/mobileapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/eurosport/mobileapp/ui/activities/MainActivity.kt
@@ -7,15 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.eurosport.domain.models.News
 import com.eurosport.mobileapp.R
 import com.eurosport.mobileapp.databinding.ActivityMainBinding
 import com.eurosport.mobileapp.ui.adapter.NewsListAdapter
 import com.eurosport.mobileapp.viewmodels.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
+
 @AndroidEntryPoint
 class MainActivity :AppCompatActivity(){
     @Inject
@@ -23,8 +25,11 @@ class MainActivity :AppCompatActivity(){
     private lateinit var viewModel:NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel= ViewModelProvider(this,viewModelFactory).get(NewsViewModel::class.java)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel= ViewModelProvider(this, viewModelFactory).get(NewsViewModel::class.java)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
         binding.viewModel = viewModel
         setContext(this);
     }
@@ -50,7 +55,7 @@ class MainActivity :AppCompatActivity(){
         @JvmStatic
         @BindingAdapter("adapter")
         fun bindNewsList(@NonNull recyclerView: RecyclerView, viewModel: NewsViewModel) {
-            val adapter = NewsListAdapter(context,viewModel.news)
+            val adapter = NewsListAdapter(context, viewModel.news)
             recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
             recyclerView.adapter = adapter
         }
